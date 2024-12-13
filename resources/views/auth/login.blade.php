c9914c86e8371a56d6f42d0350cff19e57943e7d
rizkypratama0323 <rizkyfratama.rf3@gmail.com>
2024-12-14 01:14:08 +0800

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -66,15 +66,15 @@
 				</div>
 				<div
 					class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom"
-					data-background="../assets/img/unsplash/login-bg.jpg">
+					data-background="../assets/img/unsplash/Asikboy.jpg">
 					<div class="absolute-bottom-left index-2">
 						<div class="text-light p-5 pb-2">
 							<div class="mb-5 pb-3">
 								<h1 class="mb-2 display-4 font-weight-bold" id="greetings"></h1>
-								<h5 class="font-weight-normal text-muted-transparent">Bali, Indonesia</h5>
+								<h5 class="font-weight-normal text-muted-transparent">Banjarmasin, Indonesia</h5>
 							</div>
-							Photo by <a class="text-light bb" target="_blank" href="https://unsplash.com/photos/a8lTjWJJgLA">Justin
-								Kauffman</a> on <a class="text-light bb" target="_blank" href="https://unsplash.com">Unsplash</a>
+							AXIOO-2023 <a class="text-light bb" target="_blank" href="">Rizky dan
+								Raihan</a> Lagi <a class="text-light bb" target="_blank" href="">Ambis</a>
 						</div>
 					</div>
 				</div>
